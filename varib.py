f7f3792acb5a0ac8b2d61769f40a7d4dbc3214c7
Thanush <ypthanusha70@gmail.com>
2026-07-06 09:33:09 +0530

    new commit

---
+++ varib.py
@@ -0,0 +1,9 @@
+username = input("username:")
+followers = int(input("followers"))
+verified = input("verified")
+rating = float(input("rating:"))
+
+print(username)
+print(followers)
+print(verified)
+print(rating)
